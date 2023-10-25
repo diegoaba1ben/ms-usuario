b6f0bbcedfedaf574451e4064621d8b1126769be
Diego Benjumea <diegobenjumeafreelance@outlook.com>
2023-10-25 14:14:04 -0500

    1.1 lb4 context  validación de atrib usuario y rol

--- src/models/rol.model.ts
+++ src/models/rol.model.ts
@@ -1,4 +1,4 @@
-import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Entity, hasMany, model, property} from '@loopback/repository';
 import {RolUsuario} from './rol-usuario.model';
 
 @model()
@@ -13,12 +13,28 @@ export class Rol extends Entity {
   @property({
     type: 'string',
     required: true,
+    jsonSchema: {
+      minLength: 3, //Longitud mínima.
+      maxLength: 10, // Máximo 10 caracteres
+      pattern: '^[A-Za-z]+$', //Solo letras
+      errorMessage: {
+        pattern: 'El nombre del rol debe contener solamente letras.'
+      },
+    },
   })
   nombreRol: string;
 
   @property({
     type: 'string',
     required: true,
+    jsonSchema: {
+      minLength: 3, // Longitud mínima
+      maxLength: 50, // Longitud máxima.
+      pattern: '[A-Za-z]+$',// Solo letras en el campo
+      errorMessage: {
+        pattern: 'La descripción de los roles debe contener letras'
+      }
+    }
   })
   descripcion: string;
 
--- src/models/usuario.model.ts
+++ src/models/usuario.model.ts
@@ -1,4 +1,4 @@
-import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Entity, hasMany, model, property} from '@loopback/repository';
 import {RolUsuario} from './rol-usuario.model';
 
 @model()
@@ -13,24 +13,50 @@ export class Usuario extends Entity {
   @property({
     type: 'string',
     required: true,
+    jsonSchema: {
+      minLength: 3, //Longitud mínima.
+      pattern: '^[A-Za-zzÀ-ÿ]+$', //Solo letras, incluso de otros idiomas
+      errorMessage: {
+        pattern: 'El nombre debe contener únicamente letras.'
+      }
+
+    }
   })
   nombre: string;
 
   @property({
     type: 'string',
     required: true,
+    jsonSchema: {
+      minLength: 3, //Longitud mínima
+      pattern: '^[A-Za-zÀ-ÿ]+$', //Solo letras, incluso de otros idiomas.
+      errorMessage: {
+        pattern: 'El apellido debe contener solo letras.'
+      }
+    }
   })
   apellido: string;
 
   @property({
     type: 'string',
     required: true,
+    jsonSchema: {
+      format: 'email', //Define el formato de correo electrónico
+    }
   })
   email: string;
 
   @property({
     type: 'string',
     required: true,
+    jsonSchema: {
+      minLength: 8, // Mínimo 8 caracteres
+      maxLength: 12, // Máximo 12 caracteres
+      pattern: '/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,12}$/', // Expresión regular para las reglas de validación
+      errorMessage: {
+        pattern: 'La contraseña debe tener entre 8 y 12 caracteres, incluyendo al menos una letra minúscula, una letra mayúscula, un número y un carácter especial.',
+      },
+    },
   })
   password: string;
 
